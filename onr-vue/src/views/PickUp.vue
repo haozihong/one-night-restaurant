be5785ebfe03109efb7bb138253ee0c93c1d58aa
haozihong <haozihong@hotmail.com>
2021-04-28 14:06:22 -0400

    [frontend] implement pickUp page: search pending order and pick up order

--- onr-vue/src/views/PickUp.vue
+++ onr-vue/src/views/PickUp.vue
@@ -1,12 +1,63 @@
 <template>
   <div>
-    PickUp
+    <el-form :inline="true" :model="searchForm">
+      <el-form-item label="Enter Phone:">
+        <el-input v-model="searchForm.phone" placeholder="Customer's Phone"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button @click="searchPendingOrder">Search</el-button>
+      </el-form-item>
+    </el-form>
+
+    <p v-if="!order" style="text-align: center; color: #999">
+      Enter customer's phone number and search.
+    </p>
+    <el-row>
+      <el-col :md="12">
+        <el-card v-if="order">
+          <div slot="header">
+            Customer: {{ this.order.customer.name }}
+            <el-divider direction="vertical"></el-divider>
+            Phone: {{ this.order.customer.phone }}
+          </div>
+          <p v-for="orderFoods in this.order.foodsList" :key="orderFoods.id">
+            {{ orderFoods.food.name + " × " + orderFoods.number }}
+          </p>
+          <el-divider></el-divider>
+          <p>
+            {{ "Total: $" + this.order.orderPrice }}
+          </p>
+          <el-button type="primary" style="width: 100%" @click="pickUpOrder">Pick Up</el-button>
+        </el-card>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
 export default {
-  name: "PickUp"
+  name: "PickUp",
+  data() {
+    return {
+      searchForm: { phone: "" },
+      order: null
+    }
+  },
+  methods: {
+    searchPendingOrder() {
+      if (this.searchForm.phone.length === 0)
+        return this.$message.error({ message: "Please enter customer's phone number." });
+      this.axios.get(`/orders?phone=${this.searchForm.phone}&status=0`).then(resp => {
+        // console.log(resp);
+        this.order = resp[0];
+      });
+    },
+    pickUpOrder() {
+      this.axios.put(`/orders/pickup/${this.order.id}`).then(() => {
+        this.order = null;
+      });
+    }
+  }
 }
 </script>
 
