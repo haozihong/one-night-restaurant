c7fbe36dca051eb62fcb21b486e185ce64241802
haozihong <haozihong@hotmail.com>
2021-04-28 04:23:43 -0400

    [frontend] implement food card; implement foods operation; implement summary card

--- onr-vue/src/views/NewOrder.vue
+++ onr-vue/src/views/NewOrder.vue
@@ -5,15 +5,73 @@
         <el-input v-model="searchForm.phone" placeholder="9998887777"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button type="primary" @click="searchCustomerByPhone">Search</el-button>
-        <el-button type="primary" @click="newCustomerFormVisible = true">New Customer</el-button>
+        <el-button @click="searchCustomerByPhone">Search</el-button>
+        <el-button type="success" @click="newCustomerFormVisible = true">New Customer</el-button>
       </el-form-item>
     </el-form>
 
-    <div>
-      Customer: <span>{{ currentCustomer.name }}</span>
-      Phone: <span>{{ currentCustomer.phone }}</span>
-    </div>
+    <el-row>
+      <el-col :sm="8" :xs="12">
+        Customer: <span>{{ currentCustomer.name }}</span>
+      </el-col>
+      <el-col :sm="8" :xs="12">
+        Phone: <span>{{ currentCustomer.phone }}</span>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="20">
+      <el-col :md="18">
+        <el-card shadow="hover">
+          <el-table :data="foods" :empty-text="tableEmptyText" :key="doTableUpdate">
+            <el-table-column prop="id" width="30"/>
+            <el-table-column prop="name" label="Name"/>
+            <el-table-column prop="price" label="Unit Price ($)"/>
+            <el-table-column prop="totalPrice" label="Total ($)"/>
+            <el-table-column prop="number" label="Amount" width="100"/>
+            <el-table-column width="150">
+              <template slot-scope="scope">
+                <el-button
+                    size="small"
+                    type="success"
+                    icon="el-icon-plus"
+                    plain
+                    circle
+                    @click="changeFoodNum(scope.row, 1)"></el-button>
+                <el-button
+                    size="small"
+                    type="danger"
+                    icon="el-icon-minus"
+                    plain
+                    circle
+                    @click="changeFoodNum(scope.row, -1)"></el-button>
+              </template>
+            </el-table-column>
+          </el-table>
+        </el-card>
+      </el-col>
+
+      <el-col :md="6">
+        <el-card shadow="hover">
+          <div slot="header">
+            Summary
+          </div>
+          <div v-if="foodsNeeded.length === 0" style="color: #999">
+            Empty Cart
+          </div>
+          <div v-for="food in this.foodsNeeded" :key="food.id">
+            {{ food.name + " × " + food.number }}
+          </div>
+          <hr v-if="foodsNeeded.length > 0">
+          <div v-if="foodsNeeded.length > 0">
+            {{ "Total: $" + this.foods.reduce((a, b) => a + b.totalPrice, 0) }}
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+
+    <el-row>
+      <el-button type="primary" style="width: 100%">Submit Order</el-button>
+    </el-row>
 
     <!--New customer form-->
     <el-dialog title="Add New Customer" :visible.sync="newCustomerFormVisible">
@@ -50,30 +108,60 @@ export default {
         phone: ''
       },
       newCustomerFormVisible: false,
+      foods: [],
+      foodsNeeded: [],
+      tableEmptyText: "No Data",
+      foodsNum: {},
+      doTableUpdate: 0
     }
   },
+  computed: {
+  },
+  mounted() {
+    this.initAllFoods();
+  },
   methods: {
+    initAllFoods() {
+      this.tableEmptyText = "Fetching Data...";
+      this.axios.get("/foods").then(resp => {
+        // console.log(resp);
+        if (resp) {
+          this.foods = resp;
+          this.foods.forEach(e => e.number = e.totalPrice = 0);
+        }
+        this.tableEmptyText = "No Data";
+      });
+    },
     searchCustomerByPhone() {
       this.axios.get(`/customers?phone=${this.searchForm.phone}`).then(resp => {
-        console.log(resp);
+        // console.log(resp);
         if (resp && resp[0] != null) {
           this.currentCustomer = resp[0];
         }
       });
     },
     addCustomer() {
-      console.log(this.newCustomerForm);
+      // console.log(this.newCustomerForm);
       this.axios.post(`/customers`, this.newCustomerForm).then(resp => {
         if (resp) {
           this.newCustomerFormVisible = false;
           this.currentCustomer = this.newCustomerForm;
         }
       });
+    },
+    changeFoodNum(food, delta) {
+      food.number += delta;
+      if (food.number < 0) food.number = 0;
+      food.totalPrice = food.price * food.number;
+      this.foodsNeeded = this.foods.filter(e => e.number > 0);
+      this.doTableUpdate = Math.random();
     }
   }
 }
 </script>
 
 <style scoped>
-
+.el-col {
+  margin-bottom: 1rem;
+}
 </style>
