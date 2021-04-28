b098c1685712fab4641c69a762ee74d00b4058fc
haozihong <haozihong@hotmail.com>
2021-04-27 22:35:06 -0400

    [backend] implement customer controller and test

---
+++ onr-api/src/main/java/com/zihong/onr/controller/CustomerController.java
@@ -0,0 +1,52 @@
+package com.zihong.onr.controller;
+
+import com.zihong.onr.pojo.Customer;
+import com.zihong.onr.service.CustomerService;
+import com.zihong.onr.utils.RespUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Zihong Hao
+ * @github https://github.com/haozihong
+ * @date 2021-04-27 22:08
+ */
+@RestController
+public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @GetMapping("/customers")
+    public List<Customer> getCustomers(String phone) {
+        System.out.println("[MY DEBUG] phone=>" + phone);
+        if (phone == null) {
+            return customerService.getAllCustomers();
+        } else {
+            return Arrays.asList(customerService.selectByPhone(phone));
+        }
+    }
+
+    @GetMapping("/customers/{id}")
+    public Customer selectByPrimaryKey(@PathVariable("id") Integer id) {
+        return customerService.selectByPrimaryKey(id);
+    }
+
+    @PostMapping("/customers")
+    public RespUtils insert(Customer customer) {
+        if (customerService.insert(customer) == 1) {
+            return RespUtils.ok("[SUCCESS] insert customer[" + customer.getName() + "] success");
+        }
+        return RespUtils.error("[FAIL] insert customer[" + customer.getName() + "] fail");
+    }
+
+    @DeleteMapping("/customers/{id}")
+    public RespUtils deleteByPrimaryKey(@PathVariable("id") Integer id) {
+        if (customerService.deleteByPrimaryKey(id) == 1) {
+            return RespUtils.ok("[SUCCESS] delete customer#" + id + " success");
+        }
+        return RespUtils.error("[FAIL] delete customer#" + id + " fail");
+    }
+}
