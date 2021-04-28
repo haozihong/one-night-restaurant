698954fefbb28e7b1379bc0f04af824dd07ab7e5
haozihong <haozihong@hotmail.com>
2021-04-27 21:38:49 -0400

    [backend] add main entry class

---
+++ onr-api/src/main/java/com/zihong/onr/OnrApiApplication.java
@@ -0,0 +1,11 @@
+package com.zihong.onr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnrApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OnrApiApplication.class, args);
+    }
+}
