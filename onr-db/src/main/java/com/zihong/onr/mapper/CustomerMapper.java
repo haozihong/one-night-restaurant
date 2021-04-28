9cccf0c2cbf1d35b5afab94d7e8b631b2aa2d8d5
haozihong <haozihong@hotmail.com>
2021-04-27 22:34:32 -0400

    [backend] implement customer's dao, mapper, and service layer

---
+++ onr-db/src/main/java/com/zihong/onr/mapper/CustomerMapper.java
@@ -0,0 +1,24 @@
+package com.zihong.onr.mapper;
+
+import com.zihong.onr.pojo.Customer;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * @author Zihong Hao
+ * @github https://github.com/haozihong
+ * @date 2021-04-27 21:56
+ */
+@Mapper
+public interface CustomerMapper {
+    List<Customer> getAllCustomers();
+
+    Customer selectByPrimaryKey(Integer id);
+
+    Customer selectByPhone(String phone);
+
+    int insert(Customer record);
+
+    int deleteByPrimaryKey(Integer id);
+}
---
+++ onr-db/src/main/java/com/zihong/onr/service/CustomerService.java
@@ -0,0 +1,40 @@
+package com.zihong.onr.service;
+
+import com.zihong.onr.mapper.CustomerMapper;
+import com.zihong.onr.pojo.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author Zihong Hao
+ * @github https://github.com/haozihong
+ * @date 2021-04-27 22:03
+ */
+@Service
+public class CustomerService {
+    @Autowired
+    private CustomerMapper customerMapper;
+
+    public List<Customer> getAllCustomers() {
+        return customerMapper.getAllCustomers();
+    }
+
+    public Customer selectByPrimaryKey(Integer id) {
+        return customerMapper.selectByPrimaryKey(id);
+    }
+
+    public Customer selectByPhone(String phone) {
+        return customerMapper.selectByPhone(phone);
+    }
+
+    public int insert(Customer record) {
+        return customerMapper.insert(record);
+    }
+
+    public int deleteByPrimaryKey(Integer id) {
+        return customerMapper.deleteByPrimaryKey(id);
+    }
+
+}
